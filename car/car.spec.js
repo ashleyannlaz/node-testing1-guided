725fc8f8e43f987f65412dc34a8dcfb88df8ec9d
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-08-18 18:37:51 +0200

    wip

--- car/car.spec.js
+++ car/car.spec.js
@@ -1,7 +1,11 @@
-const {} = require('./car')
+const { badCar } = require('./car')
 
 test('that it werkz', () => { // this is a test
   // empty test is a FALSE POSITIVE
   expect({}).toEqual({}) // this is an assertion
   expect(true).not.toBe(false) // this is another assertion
+})
+
+describe('badCar', () => {
+  
 })

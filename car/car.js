6640a5c6af5089669b059e4b9da38bbaf5ea529f
ashleyannlaz <paperpastels@gmail.com>
2021-09-16 20:47:03 -0400

    Finished Project

--- car/car.js
+++ car/car.js
@@ -1,1 +1,19 @@
-// Build a Car class!
+
+
+class Car {
+  constructor(make, model) {
+    this.make = make
+    this.model = model
+    this.odometer = 0
+  } drive(mileage) {
+    this.odometer += mileage
+    return mileage
+  } async driveAsync(mileage) {
+    this.odometer += mileage
+    return mileage
+  }
+}
+
+module.exports = {
+  Car,
+};
--- car/car.spec.js
+++ car/car.spec.js
@@ -1,1 +1,60 @@
-// Test away!
+const { badCar, Car } = require("./car");
+
+function objectMaker() {
+  return {};
+}
+
+test("Sanity Check", () => {
+  // this is a test
+  //false positive / this is an assertion
+  expect(true).toBe(true);
+  expect(true).not.toBe(false);
+});
+
+test("Object maker", () => {
+  const whatItReturns = objectMaker();
+  const whatWeExpect = {};
+  expect(whatItReturns).toEqual(whatWeExpect);
+});
+
+describe("Car", () => {
+    let prius 
+    beforeEach(() => {
+        prius = new Car('Toyota', 'Prius')
+    })
+  test("Car is defined", () => {
+    expect(Car).toBeDefined();
+  });
+  test("Car creates instances of cars", () => {
+    expect(prius.make).toBe("Toyota");
+    expect(prius.model).toBe("Prius");
+    expect(prius).toHaveProperty("make");
+    expect(prius).toMatchObject({ make: "Toyota", model: "Prius" });
+  });
+  test('New cars have an odometer set at zero', () => {
+      expect(prius.odometer).toBe(0)
+  })
+  test('Cars have a drive method', () => {
+    expect(prius.drive).toBeDefined()
+    expect(prius.drive).toBeInstanceOf(Function)
+    expect(prius.drive).toBe(Car.prototype.drive)
+  })
+  test('Driving the car increases odometer by driven distance', () => {
+    prius.drive(50)  
+    expect(prius.odometer).toBe(50)
+    prius.drive(50)
+    expect(prius.odometer).toBe(100)
+  })
+
+  test('Driving the car returns the driven distance', () => {
+      expect(prius.drive(50)).toBe(50)
+  })
+
+  test('driveAsync returns the driven distance asynchronously', async () => {
+      const drivenDistance = await prius.driveAsync(50)
+      expect(drivenDistance).toBe(50)
+  }, 500)
+});
+
+
+
